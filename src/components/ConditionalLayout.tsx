bb0c02fb8213a692049cd241bfb8abb6535cb6a6
Ankit-AJ3 <ankitjangid8501@gmail.com>
2025-07-10 14:03:42 +0530

    fixing some issue

--- src/components/ConditionalLayout.tsx
+++ src/components/ConditionalLayout.tsx
@@ -4,17 +4,12 @@ import { usePathname } from 'next/navigation'
 import React from 'react'
 import { Header } from '@/components/Header'
 import Footer from '@/components/Footer'
-import { useHeaderHeight } from '@/hooks/useHeaderHeight'
-
 interface ConditionalLayoutProps {
   children: React.ReactNode
 }
 
 const ConditionalLayout: React.FC<ConditionalLayoutProps> = ({ children }) => {
   const pathname = usePathname()
-  
-  // Use the header height hook for non-admin routes
-  const headerHeight = useHeaderHeight()
   
   // If pathname is undefined/null (like in not-found pages) or starts with /admin, don't show header/footer
   if (!pathname || pathname.toLowerCase().startsWith('/admin')) {
